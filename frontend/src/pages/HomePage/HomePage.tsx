9e6ee1f9ab714b98e61cac23de4d21aca1c183fd
Evgenii <eugene.pokalyuk@gmail.com>
2023-09-25 20:21:19 +0700

    create Home Page

---
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,9 @@
+import Constructor from '../../components/Constructor/Constructor';
+
+import styles from "./HomePage.module.css";
+
+export const HomePage = () => (
+    <main className={styles.container}>
+        <Constructor />
+    </main>
+);
