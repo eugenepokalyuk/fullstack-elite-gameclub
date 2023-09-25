5b373b979eb67f3943f2905d5d4ebfe700a7c6da
Evgenii <eugene.pokalyuk@gmail.com>
2023-09-25 20:24:17 +0700

    Update App.tsx
    Add Router, replace component to pages

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './App.module.css';
-import Header from '../AppHeader/AppHeader';
+import AppHeader from '../AppHeader/AppHeader';
 import Constructor from '../Constructor/Constructor';
+import { HomePage } from '../../pages/HomePage/HomePage';
+import { SettingsPage } from '../../pages/SettingsPage/SettingsPage';
+import { DEFAULT_PATH, SETTINGS_PATH } from '../../utils/routePath';
+import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
 const App = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const [loading, isLoading] = useState<boolean>(Boolean);
+  const background = location.state && location.state.background;
+
   return (
-    <main className={styles.main}>
-      <Header />
-      <Constructor />
-    </main>
+    <>
+      <AppHeader />
+      <Routes location={background || location}>
+        <Route path={DEFAULT_PATH} element={<HomePage />} />
+        <Route path={SETTINGS_PATH} element={<SettingsPage />} />
+      </Routes>
+    </>
   );
 }
 
