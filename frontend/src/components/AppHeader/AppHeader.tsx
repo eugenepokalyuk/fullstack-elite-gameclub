82f4012c86ae8389731052338ad6849fc2931b8d
Evgenii <eugene.pokalyuk@gmail.com>
2023-09-25 20:23:49 +0700

    create navigation router links

--- frontend/src/components/AppHeader/AppHeader.tsx
+++ frontend/src/components/AppHeader/AppHeader.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import styles from './AppHeader.module.css';
 import image from '../../images/logo.jpeg'
+import { NavLink, useMatch } from 'react-router-dom';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+
 const AppHeader = () => {
     return (
         <header className={styles.header}>
@@ -8,7 +13,18 @@ const AppHeader = () => {
                 <div className={styles.imageContainer}>
                     <img src={image} alt="elite logo" />
                 </div>
-                <h1>Привет, Админ!</h1>
+
+                <div className={styles.linksContainer}>
+                    <NavLink to="/" className={styles.link}>
+                        Главная
+                    </NavLink>
+
+                    <NavLink to="/settings" className={styles.link}>
+                        Настройки
+                    </NavLink>
+                </div>
+
+                <FontAwesomeIcon icon={faUser} size="2x" className={styles.user}/>
             </nav>
         </header>
     );
