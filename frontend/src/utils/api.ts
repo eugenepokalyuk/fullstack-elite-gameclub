181767e04a3e16e7d7768f4260a6c40d8c05e680
Evgenii <eugene.pokalyuk@gmail.com>
2023-10-06 01:46:08 +0700

    Update api.ts

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,24 +1,31 @@
-import data from './ping.json'
+const ApiUrlPath = 'http://localhost:8003';
 
-
-const ApiUrlPath = 'https://localhost:3001';
-// const ApiUrlPath = 'https://norma.nomoreparties.space/api';
-
-const request = (endpoint: string, options: RequestInit) => {
+const request = (endpoint: string, options: any) => {
     const url = `${ApiUrlPath}${endpoint}`;
     return fetch(url, options).then(checkResponse);
-}
-const checkResponse = (res: Response) => {
-    return res.ok ? res.json() : res.json().then((err) => Promise.reject(err));
 };
-export const fetchComputersData = () => {
-    const endpoint = '/ingredients';
-    // return request(endpoint, {})
-    //     .then((res) => {
-    //         if (res.success) return res.data;
-    //         return Promise.reject(res);
-    //     });
 
-    // mock data
-    return data;
+const checkResponse = (res: Response) => {
+    return res.ok
+        ? res.json()
+        : res.json().then((err) => Promise.reject(err));
+};
+
+export const fetchComputersData = async () => {
+    const endpoint = "/ping";
+    const options = {
+        headers: {
+            auth: 123
+        },
+    }
+    return request(endpoint, options);
+}
+export const fetchSetPlay = async () => {
+    const endpoint = "/play";
+    const options = {
+        headers: {
+            auth: 123
+        },
+    }
+    return request(endpoint, options);
 }
