f942d58f9105b421ffdf0fae7c6fb9d9004eec57
Evgenii <eugene.pokalyuk@gmail.com>
2023-10-06 01:46:29 +0700

    Update main.py

--- backend/server/main.py
+++ backend/server/main.py
@@ -3,8 +3,22 @@ from starlette.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 from assets import models
 from assets import computers
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+
 
 app = FastAPI()
+
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/ping")
 def ping():
@@ -56,3 +70,7 @@ def finish(data: models.Finish):
     except Exception as e:
         print(e)
         return JSONResponse(content='', status_code=400)
+    
+
+if __name__ == '__main__':
+    uvicorn.run(app, host="127.0.0.1", port=8003)
