af47b3798b81f8512eea12bd76d9f38d13f756c6
Evgenii <eugene.pokalyuk@gmail.com>
2023-10-06 01:46:13 +0700

    Update App.tsx

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './App.module.css';
 import AppHeader from '../AppHeader/AppHeader';
 import Constructor from '../Constructor/Constructor';
@@ -9,11 +9,43 @@ import { StatPage } from '../../pages/StatPage/StatPage';
 
 import { DEFAULT_PATH, SETTINGS_PATH, STORE_PATH, STAT_PATH } from '../../utils/routePath';
 import { Route, Routes, useLocation, useMatch, useNavigate } from 'react-router-dom';
+import { fetchComputersData } from '../../utils/api';
+import { useAppDispatch, useAppSelector } from "../../services/hooks/hooks";
+import { FETCH_COMPUTERS_FAILURE, FETCH_COMPUTERS_REQUEST, FETCH_COMPUTERS_SUCCESS, fetchComputersRequest } from '../../services/actions/computers';
+
 const App = () => {
+  const [computersData, setComputersData] = useState<any[]>([]);
+
   const location = useLocation();
   const navigate = useNavigate();
   const [loading, isLoading] = useState<boolean>(Boolean);
   const background = location.state && location.state.background;
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch({ type: FETCH_COMPUTERS_REQUEST });
+    isLoading(true);
+
+    fetchComputersData()
+      .then(res => {
+        dispatch({ type: FETCH_COMPUTERS_SUCCESS, payload: res });
+      })
+      .catch(error => {
+        dispatch({ type: FETCH_COMPUTERS_FAILURE, payload: error });
+      });
+
+    // const getComputersData = async () => {
+    //   try {
+    //     const data = await fetchComputersData();
+    //     setComputersData(data);
+    //   } catch (error) {
+    //     // Обработка ошибки
+    //   }
+    // };
+
+    // getComputersData();
+    // dispatch(fetchComputersRequest());
+  }, [dispatch]);
 
   return (
     <>
