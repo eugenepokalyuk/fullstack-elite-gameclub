9aa53ec974aed500cbbacd68e2440fd08c45a9ef
Evgenii <eugene.pokalyuk@gmail.com>
2023-10-06 01:46:12 +0700

    Update Constructor.tsx

--- frontend/src/components/Constructor/Constructor.tsx
+++ frontend/src/components/Constructor/Constructor.tsx
@@ -2,20 +2,16 @@ import React, { FC, useEffect, useState } from 'react';
 import styles from './Constructor.module.css';
 import ConstructorItem from '../ConstructorItem/ConstructorItem';
 import ConstructorPlayStation from '../ConstructorPlayStation/ConstructorPlayStation';
-import { fetchComputersData } from '../../utils/api';
-
+import { useAppSelector } from '../../services/hooks/hooks';
 
 const Constructor: FC = () => {
-    const [computersData, setComputersData] = useState<any[]>([]);
-
-    useEffect(() => {
-        const data = fetchComputersData();
-        setComputersData(data);
-    }, []);
+    const playground = useAppSelector(
+        (store) => store.playground.computers
+    );
 
     return (
         <article className={styles.section}>
-            {computersData.length > 0 && computersData.map((item: any, index: number) => {
+            {playground.length > 0 && playground.map((item: any, index: number) => {
                 return (
                     <ConstructorItem item={item} index={index + 1} key={index} />
                 )
