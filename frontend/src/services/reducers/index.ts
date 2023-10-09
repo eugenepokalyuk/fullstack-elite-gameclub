9b0270202894b4522a469c7cc5206a35c11a3d45
Evgenii <eugene.pokalyuk@gmail.com>
2023-10-09 16:10:07 +0700

    Update index.ts
    Add reducer for payment

--- frontend/src/services/reducers/index.ts
+++ frontend/src/services/reducers/index.ts
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import { computersReducer } from './computers';
 import { storeReducer } from './store';
 import { warehouseReducer } from './warehouse';
+import { paymentReducer } from './payment';
 
 const rootReducer = combineReducers({
     playground: computersReducer,
     store: storeReducer,
-    warehouse: warehouseReducer
+    warehouse: warehouseReducer,
+    payment: paymentReducer
 });
 
 export default rootReducer;
