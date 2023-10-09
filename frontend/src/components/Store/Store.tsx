95242861f3840e6f65a589eda67ab210a8fed105
Evgenii <eugene.pokalyuk@gmail.com>
2023-10-09 16:11:34 +0700

    Update Store.tsx
    refactor handle add tot cart

--- frontend/src/components/Store/Store.tsx
+++ frontend/src/components/Store/Store.tsx
@@ -1,16 +1,32 @@
 import React, { FC, useState, useEffect } from 'react';
 import styles from "./Store.module.css";
-import { useAppSelector } from '../../services/hooks/hooks';
+import { useAppDispatch, useAppSelector } from '../../services/hooks/hooks';
 import { TStoreItem } from '../../services/types/types';
-import { fetchStoreSell } from '../../utils/api';
+import { fetchStoreData, fetchStoreSell } from '../../utils/api';
 import { PaymentSwitcher } from '../PaymentSwitcher/PaymentSwitcher';
+import { FETCH_STORE_FAILURE, FETCH_STORE_REQUEST, FETCH_STORE_SUCCESS } from '../../services/actions/store';
 
 export const Store: FC = () => {
     const [selectedItems, setSelectedItems] = useState<any[]>([]);
     const [totalPrice, setTotalPrice] = useState<number>(0);
-    const [paymentType, setPaymentType] = useState<string>("card");
+
     const [itemCounts, setItemCounts] = useState<Record<number, number>>({});
     const storeItems = useAppSelector((store) => store.store.items.filter((item: any) => item.qty > 0 && item.hide === false));
+    const paymentType = useAppSelector((store) => store.payment.paymentType)
+    const dispatch = useAppDispatch();
+    const [error, setError] = useState<boolean>(false)
+    const [errorDesription, setErrorDesription] = useState<string>('')
+
+    const storeRender = async () => {
+        dispatch({ type: FETCH_STORE_REQUEST });
+        await fetchStoreData()
+            .then(res => {
+                dispatch({ type: FETCH_STORE_SUCCESS, payload: res });
+            })
+            .catch(error => {
+                dispatch({ type: FETCH_STORE_FAILURE, payload: error });
+            });
+    }
 
     useEffect(() => {
         // При изменении выбранных товаров пересчитываем общую стоимость и общее количество
@@ -21,7 +37,9 @@ export const Store: FC = () => {
             return total + product.price * count;
         }, 0);
         setTotalPrice(price);
-    }, [selectedItems, itemCounts]);
+
+        storeRender();
+    }, [selectedItems, itemCounts, dispatch]);
 
     const handleItemClick = (itemId: any) => {
         if (selectedItems.includes(itemId)) {
@@ -30,14 +48,12 @@ export const Store: FC = () => {
             setSelectedItems([...selectedItems, itemId]);
         }
     };
-
     const handleIncrement = (itemId: any) => {
         setItemCounts((prevCounts) => ({
             ...prevCounts,
             [itemId]: (prevCounts[itemId] || 0) + 1
         }));
     };
-
     const handleDecrement = (itemId: number) => {
         setItemCounts((prevCounts) => {
             const currentCount = prevCounts[itemId] || 0;
@@ -48,28 +64,28 @@ export const Store: FC = () => {
             };
         });
     };
-
-    const handleAddToCart = () => {
+    const handleAddToCart = async () => {
+        let qty = 0;
         const selectedProducts = storeItems.filter((item: TStoreItem) => selectedItems.includes(item.id))
             .map((item: TStoreItem) => {
-                const count = itemCounts[item.id] || 0;
-                return { ...item, count };
+                qty = itemCounts[item.id] || 0;
+                return { ...item, qty };
             });
-
         const data = {
             items: selectedProducts,
             payment: paymentType
         };
 
-        fetchStoreSell(data)
-            .then(res => {
-                console.log({ res })
-            })
-            .catch(error => {
-                console.log({ error })
-            });
-        // setModalOpen(false);
-        setSelectedItems([]);
+        if (qty > 0) {
+            fetchStoreSell(data)
+                .then(res => { })
+                .catch(error => {
+                    setError(true);
+                    setErrorDesription("Неверно указано кол-во товаров")
+                });
+            setSelectedItems([]);
+            storeRender();
+        }
     };
 
     return (
@@ -110,7 +126,6 @@ export const Store: FC = () => {
                             {selectedItems.map((itemId) => {
                                 const item = storeItems.find((item: TStoreItem) => item.id === itemId);
                                 return (
-
                                     <li className={styles.cartRow} key={itemId}>
                                         {item.name}
                                         <div className={styles.cartRow}>
@@ -122,9 +137,15 @@ export const Store: FC = () => {
                                 )
                             })}
                         </ul>
-                    ) : (
-                        <p>Корзина пуста</p>
-                    )}
+                    )
+                        : error
+                            ? <>
+                                <p className={styles.warningMessage}>{errorDesription}</p>
+                            </>
+                            : (
+                                <p>Корзина пуста</p>
+                            )
+                    }
                 </div>
 
                 <div className={styles.cartFooter}>
