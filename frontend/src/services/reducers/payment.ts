04fff64fc1f00dbd999eff7a74192a940b3d1e7b
Evgenii <eugene.pokalyuk@gmail.com>
2023-10-09 16:10:04 +0700

    Create payment.ts
    Add reducer for payment

---
+++ frontend/src/services/reducers/payment.ts
@@ -0,0 +1,29 @@
+import {
+    SWITCH_PAYMENT_REQUEST,
+} from "../actions/payment";
+
+const initialState: any = {
+    paymentType: "card",
+    loading: false,
+    error: null,
+};
+
+export const selectLoading = (state: any) => state.computers.loading;
+export const selectError = (state: any) => state.computers.error;
+
+export const paymentReducer = (
+    state = initialState,
+    action: any
+) => {
+    switch (action.type) {
+        case SWITCH_PAYMENT_REQUEST:
+            return {
+                ...state,
+                paymentType: action.payload,
+                loading: false,
+                error: null,
+            };
+        default:
+            return state;
+    }
+};
