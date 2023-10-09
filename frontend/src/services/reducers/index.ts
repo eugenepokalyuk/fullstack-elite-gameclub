7388510d9be639c0bc4951da4866f0f4927a263d
Evgenii <eugene.pokalyuk@gmail.com>
2023-10-09 20:46:18 +0700

    Add new reducer for stat

--- frontend/src/services/reducers/index.ts
+++ frontend/src/services/reducers/index.ts
@@ -3,12 +3,14 @@ import { computersReducer } from './computers';
 import { storeReducer } from './store';
 import { warehouseReducer } from './warehouse';
 import { paymentReducer } from './payment';
+import { statReducer } from './stat';
 
 const rootReducer = combineReducers({
     playground: computersReducer,
     store: storeReducer,
     warehouse: warehouseReducer,
-    payment: paymentReducer
+    payment: paymentReducer,
+    stat: statReducer
 });
 
 export default rootReducer;
