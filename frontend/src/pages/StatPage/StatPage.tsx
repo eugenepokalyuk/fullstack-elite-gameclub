fb12c7d51e4abcbad4e5feef1b0ef0fbe3ec1e59
Evgenii <eugene.pokalyuk@gmail.com>
2023-10-09 20:47:03 +0700

    Move logic data to single component
    from Stat page to Stat Constructor component

--- frontend/src/pages/StatPage/StatPage.tsx
+++ frontend/src/pages/StatPage/StatPage.tsx
@@ -1,42 +1,11 @@
+import { StatConstructor } from "../../components/StatConstructor/StatConstructor";
 import styles from "./StatPage.module.css";
-import { fetchStatPC, fetchStatStore } from "../../utils/api";
 
 export const StatPage = () => {
-    const from = "2019-08-24T14:15:22Z";
-    const until = "2024-08-24T14:15:22Z";
-
-    const getStatPC = () => {
-        fetchStatPC(from, until)
-            .then(res => {
-                console.log({ res });
-            })
-            .catch(error => {
-                console.log({ error });
-            });
-    }
-    const getStatStore = () => {
-        fetchStatStore(from, until)
-            .then(res => {
-                console.log({ res });
-            })
-            .catch(error => {
-                console.log({ error });
-            });
-    }
-
     return (
         <section className={styles.container}>
             <h1>Статистика</h1>
-
-            <article>
-                <button onClick={getStatPC}>
-                    getStatPC
-                </button>
-
-                <button onClick={getStatStore}>
-                    getStatStore
-                </button>
-            </article>
+            <StatConstructor />
         </section>
     )
 };
