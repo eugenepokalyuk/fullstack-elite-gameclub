f29bdc451fd46d351d609eed2302ba1f1325d098
Evgenii <eugene.pokalyuk@gmail.com>
2023-10-10 21:11:53 +0700

    Update PlaygroundGrid.tsx
    Fix bug with dnd grid 0

--- frontend/src/components/PlaygroundGrid/PlaygroundGrid.tsx
+++ frontend/src/components/PlaygroundGrid/PlaygroundGrid.tsx
@@ -4,6 +4,7 @@ import PlaygroundSquare from '../PlaygroundSquare/PlaygroundSquare';
 import { fetchComputerGridReplace, fetchComputersData } from '../../utils/api';
 import { useAppDispatch, useAppSelector } from '../../services/hooks/hooks';
 import { FETCH_COMPUTERS_FAILURE, FETCH_COMPUTERS_REQUEST, FETCH_COMPUTERS_SUCCESS } from '../../services/actions/computers';
+import { TComputer } from '../../services/types/types';
 
 const PlaygroundGrid: FC = () => {
     const dispatch = useAppDispatch();
@@ -29,7 +30,7 @@ const PlaygroundGrid: FC = () => {
     }, [dispatch])
 
     const handleDragStart = (e: React.DragEvent<HTMLDivElement>, id: number) => {
-        Number(e.dataTransfer.setData('text/plain', id.toString())); // Устанавливаем данные перетаскивания
+        Number(e.dataTransfer.setData('text/plain', `${id}`));
     };
 
     const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
@@ -37,20 +38,28 @@ const PlaygroundGrid: FC = () => {
     };
 
     const handleDrop = async (e: React.DragEvent<HTMLDivElement>, id: number) => {
-        e.preventDefault();
-        const sourceId = Number(e.dataTransfer.getData('text/plain')); // Получаем данные перетаскивания
+        let flag = false;
 
-        if (sourceId) {
-            await fetchComputerGridReplace(sourceId, id);
+        playground.map((item: TComputer) => {
+            if (id === item.grid_id)
+                flag = true;
+        })
 
-            dispatch({ type: FETCH_COMPUTERS_REQUEST });
-            fetchComputersData()
-                .then(res => {
-                    dispatch({ type: FETCH_COMPUTERS_SUCCESS, payload: res });
-                })
-                .catch(error => {
-                    dispatch({ type: FETCH_COMPUTERS_FAILURE, payload: error });
-                });
+        if (!flag) {
+            e.preventDefault();
+            const sourceId = Number(e.dataTransfer.getData('text/plain')); // Получаем данные перетаскивания
+            if (sourceId) {
+                await fetchComputerGridReplace(sourceId, id);
+
+                dispatch({ type: FETCH_COMPUTERS_REQUEST });
+                fetchComputersData()
+                    .then(res => {
+                        dispatch({ type: FETCH_COMPUTERS_SUCCESS, payload: res });
+                    })
+                    .catch(error => {
+                        dispatch({ type: FETCH_COMPUTERS_FAILURE, payload: error });
+                    });
+            }
         }
     };
 
