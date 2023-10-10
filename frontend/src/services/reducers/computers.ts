0680f5917eb2bad3f1667da77f7500edb61bc99a
Evgenii <eugene.pokalyuk@gmail.com>
2023-10-10 23:53:33 +0700

    Update computers.ts

--- frontend/src/services/reducers/computers.ts
+++ frontend/src/services/reducers/computers.ts
@@ -2,16 +2,26 @@ import {
     FETCH_COMPUTERS_REQUEST,
     FETCH_COMPUTERS_SUCCESS,
     FETCH_COMPUTERS_FAILURE,
+    // UPDATE_PC_SESSION
 } from "../actions/computers";
 
 const initialState: any = {
     computers: [],
+    playingComputers: [],
     loading: false,
     error: null,
 };
 
 export const selectLoading = (state: any) => state.computers.loading;
 export const selectError = (state: any) => state.computers.error;
+
+// export const updatePcSession = (id: string, pc_session: string) => ({
+//     type: UPDATE_PC_SESSION,
+//     payload: {
+//         id,
+//         pc_session,
+//     },
+// });
 
 export const computersReducer = (
     state = initialState,
@@ -37,6 +47,14 @@ export const computersReducer = (
                 loading: false,
                 error: action.payload,
             };
+        // case UPDATE_PC_SESSION:
+        //     const { id, pc_session } = action.payload;
+        //     let updatedComputers = [...state.computers];
+        //     updatedComputers.find(el => el.id == id).pc_session = pc_session
+        //     return {
+        //         ...state,
+        //         playingComputers: updatedComputers,
+        //     };
         default:
             return state;
     }
