f1d2388eb7dbe8e81ca67946281609bd0373f646
Evgenii <eugene.pokalyuk@gmail.com>
2023-10-10 21:07:28 +0700

    Update index.ts
    Add 3 reducers

--- frontend/src/services/reducers/index.ts
+++ frontend/src/services/reducers/index.ts
@@ -4,13 +4,17 @@ import { storeReducer } from './store';
 import { warehouseReducer } from './warehouse';
 import { paymentReducer } from './payment';
 import { statReducer } from './stat';
+import { authReducer } from './auth';
+import { sessionReducer } from './session';
 
 const rootReducer = combineReducers({
     playground: computersReducer,
     store: storeReducer,
     warehouse: warehouseReducer,
     payment: paymentReducer,
-    stat: statReducer
+    stat: statReducer,
+    auth: authReducer,
+    session: sessionReducer
 });
 
 export default rootReducer;
