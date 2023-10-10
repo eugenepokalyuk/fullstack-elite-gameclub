217c86257c2b0543e7589544699bf311d8ddfa01
Evgenii <eugene.pokalyuk@gmail.com>
2023-10-10 23:53:49 +0700

    Update SettingsPage.tsx
    Future template

--- frontend/src/pages/SettingsPage/SettingsPage.tsx
+++ frontend/src/pages/SettingsPage/SettingsPage.tsx
@@ -1,4 +1,5 @@
 import ComputerList from "../../components/ComputerList/ComputerList";
+import { PersonalSettings } from "../../components/PersonalSettings/PersonalSettings";
 import styles from "./SettingsPage.module.css";
 
 export const SettingsPage = () => (
@@ -6,6 +7,7 @@ export const SettingsPage = () => (
         <h1>Настройки</h1>
 
         <article>
+            <PersonalSettings />
             <ComputerList />
         </article>
     </section>
