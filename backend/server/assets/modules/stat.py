f821f3c680cce70312bdaf0ebd52b91fdf53b434
ohksx <ohksx10@gmail.com>
2023-10-19 21:18:43 +0700

    Update stat.py

--- backend/server/assets/modules/stat.py
+++ backend/server/assets/modules/stat.py
@@ -93,7 +93,8 @@ def get_supply_stat(start, finish, db):
 def get_device_stat(start, db):
     device_data = db.query(Orders, Pcs.name)\
         .join(Pcs, Orders.pc_id == Pcs.id)\
-        .filter(and_(Orders.start >= start, Orders.status != 'canceled')).all()
+        .where(Orders.start >= start).all()
+        # .filter(and_(Orders.start >= start, Orders.status != 'canceled')).all()
     for i, v in device_data:
         print(f'order start date: {i.start}')
     return [
